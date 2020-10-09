98d78acf0427da9bbc66a0646b4b9cfae042d4b4
MwauraKimani <kimmwaus@gmail.com>
2020-10-09 18:09:07 +0300

    comit sign in change

--- app/php/control/signin.php
+++ app/php/control/signin.php
@@ -38,6 +38,11 @@ if (isset($_POST['Submit'])) {
 
      $status = $response[0];
 
+     echo  $response[1][0]['username'];
+     echo  $response[1][0]['moderatorEmail'];
+     echo  $response[1][0]['password'];
+     exit();
+
      if($status){
        $dbuser = $response[1][0]['username'];
        $dbemail = $response[1][0]['moderatorEmail'];
